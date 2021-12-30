b5265551c437bdc18c95d5d3b11427d63d5168a6
amolnagose12 <amolnagose12@gmail.com>
2021-12-30 12:06:47 +0530

    Added service layer

--- src/main/java/com/bridgelabz/addressbookapp/service/IPersonService.java
+++ src/main/java/com/bridgelabz/addressbookapp/service/IPersonService.java
@@ -1,7 +1,22 @@
 package com.bridgelabz.addressbookapp.service;
 
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.bridgelabz.addressbookapp.dto.PersonDTO;
+import com.bridgelabz.addressbookapp.model.Person;
+
+@Service
 public interface IPersonService {
-	
-	}
 
+	Person createPersonData(PersonDTO personDTO);
+
+	Person updatePersonDta(long id, PersonDTO personDTO);
+
+	List<Person> getPersonData();
+
+	Person getPersonById(long id);
+
+	void deletePersonData(long id);
+}
--- src/main/java/com/bridgelabz/addressbookapp/service/PersonService.java
+++ src/main/java/com/bridgelabz/addressbookapp/service/PersonService.java
@@ -1,5 +1,65 @@
 package com.bridgelabz.addressbookapp.service;
 
-public class PersonService {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bridgelabz.addressbookapp.dto.PersonDTO;
+import com.bridgelabz.addressbookapp.model.Person;
+import com.bridgelabz.addressbookapp.repository.IPersonRepository;
+
+@Service
+public class PersonService implements IPersonService {
+
+	@Autowired
+	private IPersonRepository personRepository;
+
+	/**
+	 * @param PersonDTO
+	 * @return : PersonData
+	 */
+	@Override
+	public Person createPersonData(PersonDTO personDTO) {
+		Person contactData = null;
+		contactData = new Person(1, personDTO);
+		return personRepository.save(contactData);
+	}
+
+	/**
+	 * @param PersonDTO
+	 * @return : PersonData
+	 */
+	@Override
+	public Person updatePersonDta(long id, PersonDTO personDTO) {
+		Person person = this.getPersonById(id);
+		personData.updatePersonData(personDTO);
+		return personRepository.save(person);
+	}
+
+	/**
+	 * @param PersonDTO
+	 * @return : PersonData
+	 */
+	@Override
+	public List<Person> getPersonData() {
+		return personRepository.findAll();
+	}
+
+	/**
+	 * @param PersonDTO
+	 * @return : PersonData
+	 */
+	@Override
+	public Person getPersonById(long id) {
+		return personRepository.findById(id);
+	}
+
+	@Override
+	public void deletePersonData(long id) {
+		Person person = this.getPersonById(id);
+		personRepository.delete(person);
+	}
 
 }
